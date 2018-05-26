ce7e026bc2e4c93fe1cd3f95b1fbaff842f6a02c
EricDunbar <edunbar@ucsc.edu>
2018-05-25 18:39:32 -0700

    added comments and changed names

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,18 +1,29 @@
+/*
+    nav.js creates a smooth transition when mousing over one tab to the next. It uses two even listeners
+    and a background that follows the information. 
+    Attaches to index.html and home.css
+*/
 
+
+//creates the constant dom selectors that will be used later
 const triggers = document.querySelectorAll('.cool > li')
 const background = document.querySelector('.dropdownBackground')
 const nav = document.querySelector('.top')
 
+//handles the event that happens when the mouse starts to hover over the different nav selectors
 function handleEnter() {
 
+    //adds class names to trigger different events on the class tags
     this.classList.add('trigger-enter')
     setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 75)
     background.classList.add('open')
 
+    //this gets the dropdown coordinates to move the background onto the actual information
     const dropdown = this.querySelector('.dropdown')
     const dropdownCoords = dropdown.getBoundingClientRect()
     const navCoords = nav.getBoundingClientRect()
 
+    //makes a variable that has the coordinates that the background should be
     const coords = {
         height: dropdownCoords.height,
         width: dropdownCoords.width,
@@ -20,19 +31,21 @@ function handleEnter() {
         left: dropdownCoords.left - navCoords.left,
     }
 
+    //sets the style of the background based off the coords that were set previously
     background.style.setProperty('width', `${coords.width}px`)
     background.style.setProperty('height', `${coords.height}px`)
     background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px`)
 
 }
 
-
+//function that handles the mouse leaving the nav bars
 function handleLeave() {
 
+    //this removes the added class names that were added when the mouse enters the navs
     this.classList.remove('trigger-enter', 'trigger-enter-active')
     background.classList.remove('open')
 }
 
-
+//event listeners for the two functions that are being used
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter))
 triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave))
