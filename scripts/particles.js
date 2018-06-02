c826a286ef6e1b5df5dd65483857f9b2bc683b3a
EricDunbar <edunbar@ucsc.edu>
2018-06-01 18:12:28 -0700

    Scrapped previous idea, going new direction using canvas

--- scripts/particles.js
+++ scripts/particles.js
@@ -2,32 +2,32 @@
     particles.js creates the project portion of the website. 
 */
 
-// var canvas = document.getElementById("myCanvas");
-// var ctx;
-// var x = 150, y = 100;
-// var dx = 1, dy = 1;
+var canvas = document.getElementById("canvas");
+var ctx;
+var x = 150, y = 100;
+var dx = 1, dy = 1;
 
-// function init() {
-//     ctx = canvas.getContext("2d");
-//     return setInterval(draw, 10);
-// }
+function init() {
+    ctx = canvas.getContext("2d");
+    return setInterval(draw, 10);
+}
 
-// function draw() {
-//     ctx.clearRect(0,0,300, 300);
-//     ctx.beginPath();
-//     ctx.arc(x, y, 20, 0, Math.PI*2, true); 
-//     ctx.closePath();
-//     ctx.fill();
-//     x += dx;
-//     y += dy;
-//     if (x > 300 || x < 0) {
-//          dx *= -1;   
-//     }
-//     if (y > 150 || y < 0) {
-//          dy *= -1;   
-//     }
-// }
-// init();
+function draw() {
+    ctx.clearRect(0,0,300, 300);
+    ctx.beginPath();
+    ctx.arc(x, y, 20, 0, Math.PI*2, true); 
+    ctx.closePath();
+    ctx.fill();
+    x += dx;
+    y += dy;
+    if (x > 300 || x < 0) {
+         dx *= -1;   
+    }
+    if (y > 150 || y < 0) {
+         dy *= -1;   
+    }
+}
+init();
 
 
 
