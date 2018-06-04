c88dd5442292bf72b67f98b2d1ffec302a0308fc
EricDunbar <edunbar@ucsc.edu>
2018-06-04 16:36:03 -0700

    Have particles constrained within two circles, need to add web effect, and info

--- scripts/particles.js
+++ scripts/particles.js
@@ -1,35 +1,76 @@
-/*
-    particles.js creates the project portion of the website. 
-*/
+// /*
+//     particles.js creates the project portion of the website. 
+// */
 
-var canvas = document.getElementById("canvas");
-var ctx;
-var x = 150, y = 100;
-var dx = 1, dy = 1;
-
-function init() {
-    ctx = canvas.getContext("2d");
-    return setInterval(draw, 10);
+function getBall(xVal, yVal, dxVal, dyVal, rVal, colorVal) {
+    var ball = {
+      x: xVal,
+      lastX: xVal,
+      y: yVal,
+      lastY: yVal,
+      dx: dxVal,
+      dy: dyVal,
+      r: rVal,
+      color: colorVal,
+      normX: 0,
+      normY: 0
+    };
+  
+    return ball;
 }
+  
+var canvas = document.getElementById("myCanvas");
+  
+  var ctx = canvas.getContext("2d");
+  
+  var containerR = 325;
+  var containerInner = containerR / 1.5;
+  
+  canvas.width = containerR * 2;
+  canvas.height = containerR * 2;
 
-function draw() {
-    ctx.clearRect(0,0,300, 300);
-    ctx.beginPath();
-    ctx.arc(x, y, 20, 0, Math.PI*2, true); 
-    ctx.closePath();
-    ctx.fill();
-    x += dx;
-    y += dy;
-    if (x > 300 || x < 0) {
-         dx *= -1;   
+  canvas.style["border-radius"] = containerR + "px";
+  
+  var balls = [
+    getBall(containerR, containerR * 2 - 30, 2, -2, 20, "#0095DD"),
+    getBall(containerR, containerR * 2 - 50, 3, -3, 30, "#DD9500"),
+    getBall(containerR, containerR * 2 - 60, -3, 4, 10, "#00DD95"),
+    getBall(containerR, containerR * 2 / 5, -1.5, 3, 40, "#DD0095")
+  ];
+  
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+    for (var i = 0; i < balls.length; i++) {
+      var curBall = balls[i];
+      ctx.beginPath();
+      ctx.arc(curBall.x, curBall.y, curBall.r, 0, Math.PI * 2);
+      ctx.fillStyle = curBall.color;
+      ctx.fill();
+      ctx.closePath();
+      curBall.lastX = curBall.x;
+      curBall.lastY = curBall.y;
+      curBall.x += curBall.dx;
+      curBall.y += curBall.dy;
+      var dx = curBall.x - containerR;
+      var dy = curBall.y - containerR;
+      var distanceFromCenter = Math.sqrt(dx * dx + dy * dy);
+  
+      if (distanceFromCenter >= containerR - curBall.r || distanceFromCenter <= containerInner - curBall.r) {
+        var normalMagnitude = distanceFromCenter;
+        var normalX = dx / normalMagnitude;
+        var normalY = dy / normalMagnitude;
+        var tangentX = -normalY;
+        var tangentY = normalX;
+        var normalSpeed = -(normalX * curBall.dx + normalY * curBall.dy);
+        var tangentSpeed = tangentX * curBall.dx + tangentY * curBall.dy;
+        curBall.dx = normalSpeed * normalX + tangentSpeed * tangentX;
+        curBall.dy = normalSpeed * normalY + tangentSpeed * tangentY;
+      }
+
     }
-    if (y > 150 || y < 0) {
-         dy *= -1;   
-    }
-}
-init();
-
-
-
-
-
+    requestAnimationFrame(draw);
+  }
+  
+  draw();
+  
